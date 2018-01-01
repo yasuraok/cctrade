2b5a3645a7b78e613869b9ffb58740496a480892
Naoki Yasuraoka <yasuraok@gmail.com>
2018-01-01 21:01:36 +0900

    Simplify database format

--- index.ts
+++ index.ts
@@ -52,24 +52,73 @@ function notify2ifttt(){
   request.post(options, function(error, response, body){});
 }
 
-class CCList{
+class CCWatch{
+  readonly interval = 60 * 1000; // 監視タイム
+
   pairs: any
   constructor(){}
 
   // zaifで現在扱っている通貨ペアのリストを取得してコールバックを呼ぶ
-  load(callback): void{
+  start(): void{
     request.get({uri: "https://api.zaif.jp/api/1/currency_pairs/all"},
-      function(error, response, body){
+      (error, response, body) => {
         // 通貨ペア一覧からJPYのものだけを取り出す
+        // "公開情報APIのcurrency_pairsで取得できるevent_numberが0であるものが指定できます" とのことなので
+        // それもフィルタリングする
         this.pairs = JSON.parse(body).filter((element) => {
-          return element.currency_pair.match("jpy");
+          const isJpy    = element.currency_pair.match("jpy");
+          const isActive = element.event_number == 0;
+          return isJpy && isActive;
         });
 
-        // for(let pair of this.pairs){
-        //   console.log(pair);
-        // }
-        callback();
-    }.bind(this));
+        // 監視スタート
+        this.watch();
+    });
+  }
+
+  // 価格情報の1レコードのjsonを作る
+  makeRecord(pair, time, price): object{
+    // date, currency_pair, price
+    return {c: pair, d: time, p: price};
+  }
+
+  // 現在価格を取得して場合によっては取引する
+  update(): void{
+    for(let c of this.pairs){
+      // 1. 価格を取得する
+      // 本当はdepthを見てスプレッドを見た方がいい
+      request.get({uri: "https://api.zaif.jp/api/1/last_price/" + c.currency_pair},
+        (error, response, body) =>{
+          // 2. 自分のDBに記録する
+          const now    = new Date();
+          const price  = JSON.parse(body).last_price;
+          const record = this.makeRecord(c.currency_pair, now.getTime(), price);
+
+          console.log(dateFormat(now), c.currency_pair, ":", price);
+          db.insert(record, (err) => {
+            // 既存記録とレコードとまとめる
+            const query = {pair: c.currency_pair, date: now};
+            db.find(query, (err, docs) => {
+              // エージェントに対して
+              const agents = [];
+              for(let agent of agents){
+                // 3. 移動平均を作る
+
+                // 4. 判断を入れる (買ってない場合->買うか何もしないか、買っている場合->買い増すか売るか何もしないか)
+
+                // 5. 通知を入れる
+
+              }
+            });
+          });
+        });
+    }
+  }
+
+  // 定期的に実行
+  watch(): void{
+    this.update()
+    setInterval(() => {this.update()}, this.interval);
   }
 }
 
@@ -78,38 +127,6 @@ check();
 // notify2ifttt();
 // currencies_all();
 
-var cl:CCList = new CCList();
-cl.load(() => {
-  // 各通貨に対して...
-  for(let c of cl.pairs){
-    // 1. 価格を取得する
-    // 本当はdepthを見てスプレッドを見た方がいい
-    request.get({uri: "https://api.zaif.jp/api/1/last_price/" + c.currency_pair},
-      (error, response, body) =>{
-        // 2. 自分のDBに記録する
-        const now    = new Date();
-        const price  = JSON.parse(body).last_price;
-        const record = {date: now.getTime(), pair: c.currency_pair, price: price};
+var ccw:CCWatch = new CCWatch();
 
-        console.log(dateFormat(now), c.currency_pair, ":", JSON.parse(body).last_price);
-        db.insert(record, (err) => {
-          // 既存記録とレコードとまとめる
-          const query = {pair: c.currency_pair, date: now};
-          db.find(query, (err, docs) => {
-            // エージェントに対して
-            const agents = [];
-            for(let agent of agents){
-              // 3. 移動平均を作る
-
-              // 4. 判断を入れる (買ってない場合->買うか何もしないか、買っている場合->買い増すか売るか何もしないか)
-
-              // 5. 通知を入れる
-
-            }
-          });
-        });
-      });
-  }
-
-  // console.log(this.pairs);
-});
+ccw.start();
